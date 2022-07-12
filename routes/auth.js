c710b6169b663b7a78193792fef9e4313ae743c0
salah deco <salah.deco1@gmail.com>
2022-07-12 14:09:52 +0100

    use crypto-js to hashed the password method  AES

--- routes/auth.js
+++ routes/auth.js
@@ -1,12 +1,13 @@
 const router = require('express').Router();
 const User = require('../models/User');
+const CryptoJS = require("crypto-js");
 
 // Register routes
 router.post("/register", async (req, res) => {
     const newUser = new User({
         username: req.body.username,
         email: req.body.email,
-        password: req.body.password
+        password: CryptoJS.AES.encrypt(req.body.password, process.env.SECRET_KEY).toString()
     });
 
     try {
@@ -18,6 +19,27 @@ router.post("/register", async (req, res) => {
         console.error(err);
         res.status(500).json(err);
     }
-})
+});
+
+// LOGIN
+router.post('/login', async (req, res) => {
+    try {
+        const user = await User.findOne({
+            username: req.body.username
+        });
+        console.log(user)
+        !user && res.status(401).json("Wrong credentials!");
+
+        const hashedPassword = CryptoJS.AES.decrypt(user.password, process.env.SECRET_KEY);
+        const decryptPassword = hashedPassword.toString(CryptoJS.enc.Utf8);
+        decryptPassword !== req.body.password && res.status(401).json("Wrong credentials!");
+        // successfull
+        const { password, ...others } = user._doc;
+        res.status(200).json(others);
+    } catch(err) {
+        console.log("error", err.message);
+        res.status(500).json(err);
+    }
+});
 
 module.exports = router;
